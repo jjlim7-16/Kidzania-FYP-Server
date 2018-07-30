8bb150509479f8f21702df28e1ee921261ac3a53
jjlim7-16 <jjlim7-16@ichat.sp.edu.sg>
2018-07-30 13:28:39 +0800

    Update again

--- routes/accountRouter.js
+++ routes/accountRouter.js
@@ -8,11 +8,81 @@ const pool = db.getPool()
 
 const router = express.Router()
 
-router.get('/:userID', function (req, res) {
-  var userID = parseInt(req.params.userID)
-  let sql = `SELECT ua.user_id, ua.account_type_id, ua.username, acct.account_type, acct.station_id
+router.options('*', cors())
+router.use(cors())
+
+
+router.route('/')
+  .all((req, res, next) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain')
+    next()
+  })
+  .get((req, res) => {
+    let sql = ` SELECT ua.user_id, ua.account_type_id, ua.username, acct.account_type,acct.station_id, s.station_name
+FROM user_accounts ua
+LEFT JOIN account_type acct ON ua.account_type_id  = acct.account_type_id
+LEFT JOIN stations s ON s.station_id = acct.station_id `
+    pool.getConnection().then(function (connection) {
+      connection.query(sql)
+        .then((rows) => {
+          res.json(rows)
+        })
+        .catch(err => {
+          res.statusMessage = err
+          res.status(400).end()
+        })
+      connection.release()
+    })
+
+  })
+  .post((req, res) => {
+    let sql =`insert into user_accounts( account_type_id,username,password_hash)values (?,?,?)`
+    let passwordhash = "";
+    let data = req.body;
+    let userData = [parseInt(data.account_type_id),data.username,passwordhash];
+    pool.getConnection().then(function(connection) {
+      connection.query(sql, userData)
+        .then((rows) => {
+          res.json(rows)
+          res.status(200).end()
+        })
+        .catch((err) => {
+          res.statusMessage = err
+          res.status(400).end()
+        })
+      connection.release()
+    })
+  })
+
+  
+router.get('/getAccountTypeCrewList', (req,res) => {
+  let sql = `SELECT a.*, st.station_name FROM account_type a
+    INNER JOIN stations st ON st.station_id = a.station_id 
+    where a.account_type = 'Crew';`
+  pool.getConnection().then(function (connection) {
+    connection.query(sql)
+      .then((rows) => {
+        res.json(rows)
+      })
+      .catch(err => {
+        res.statusMessage = err
+        res.status(400).end()
+      })
+    connection.release()
+  })
+})
+
+router.route('/:userID')
+.all((req, res, next) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  next()
+})
+.get((req, res) => {
+  let sql = `SELECT ua.user_id, ua.account_type_id, ua.username, acct.account_type,acct.station_id
   FROM user_accounts ua, account_type acct
-  where ua.account_type_id = acct.account_type_id and ua.user_id = ?`
+  where ua.account_type_id = acct.account_type_id and ua.user_id = ?;`
   //database query havent filter by date
   pool.getConnection().then(function (connection) {
     connection.query(sql, userID)
@@ -43,20 +113,8 @@ router.get('/:userID', function (req, res) {
       })
     connection.release()
   })
-  }).delete('/:userID', (req, res) =>  {
-  var userID = parseInt(req.params.userID); 
-  let sql = `Delete From user_accounts where user_id = ?`; 
-  pool.getConnection().then(function (connection) {
-    connection.query(sql, userID)
-      .then(results =>  {
-        res.json(results)
-      })
-      .catch(err =>  {
-        console.log(err)
-      })
-    connection.release()
-  })
 })
+
 
 router.get('/getListOfAccountbyAccountTypeID/:accountTypeID', function (req, res) {
   var accountTypeID = parseInt(req.params.accountTypeID)
@@ -76,62 +134,5 @@ router.get('/getListOfAccountbyAccountTypeID/:accountTypeID', function (req, res
   })
 })
 
-router.route('/')
-  .all((req, res, next) =>  {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    next()
-  })
-  .get((req, res) =>  {
-    let sql = `SELECT ua.user_id, ua.account_type_id, ua.username, acct.account_type, ua.station
- FROM user_accounts ua, account_type acct
- where ua.account_type_id = acct.account_type_id`
-    pool.getConnection().then(function (connection) {
-      connection.query(sql)
-        .then((rows) =>  {
-          res.json(rows)
-        })
-        .catch(err =>  {
-          res.statusMessage = err
-          res.status(400).end()
-        })
-    })
-  })
-
-  .post((req, res) => {
-    // console.log(req.files)
-    // console.log((req.body.webFormData))
-    let userData = req.body; 
-
-    let sql = `Insert into user_accounts(account_type_id, username, password_hash)VALUES (?)`
-    let passwordHash = "asaasas"; 
-    let userVal = [parseInt(userData.account_type_id), userData.username, passwordHash]
-    pool.getConnection().then(function (connection) {
-      connection.query(sql, [userVal])
-        .then((results) =>  {
-          res.json(results)
-        })
-        .catch((err) =>  {
-          console.log(err)
-          res.statusMessage = err
-          res.status(400).end(err.code)
-        })
-      connection.release()
-    })
-  })
-  .delete((req, res) => {
-    let sql = 'Select username From user_accounts where user_id = ' + req.params.userID + ';'
-    sql += 'Delete From user_accounts where user_id = ' + req.params.userID
-    pool.getConnection().then(function(connection) {
-      connection.query(sql)
-        .then(results => {
-          res.json(results)
-        })
-        .catch(err => {
-          console.log(err)
-        })
-      connection.release()
-    })
-  })
 
 module.exports = router
