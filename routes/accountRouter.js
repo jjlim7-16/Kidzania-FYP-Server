849cf7501e3c74ac66d18b3ee0bd0d895415cb80
JiayuMeng <JiayuMeng@users.noreply.github.com>
2018-07-30 20:47:18 +0800

    edit accountRouter

--- routes/accountRouter.js
+++ routes/accountRouter.js
@@ -26,13 +26,14 @@ router.use(bodyParser.json({
 router.options('*', cors())
 router.use(cors())
 
-
 router.route('/')
   .all((req, res, next) => {
     res.statusCode = 200
     res.setHeader('Content-Type', 'text/plain')
     next()
   })
+
+
   .get((req, res) => {
     let sql = ` SELECT ua.user_id, ua.account_type_id, ua.username, acct.account_type,acct.station_id, s.station_name
 FROM user_accounts ua
@@ -49,89 +50,7 @@ LEFT JOIN stations s ON s.station_id = acct.station_id `
         })
       connection.release()
     })
-
   })
-  .post((req, res) => {
-    let sql =`insert into user_accounts( account_type_id,username,password_hash)values (?,?,?)`
-    let passwordhash = "";
-    let data = req.body;
-    let userData = [[parseInt(data.account_type_id),data.username,passwordhash]];
-    pool.getConnection().then(function(connection) {
-      connection.query(sql, [userData])
-        .then((rows) => {
-          res.json(rows)
-          res.status(200).end()
-        })
-        .catch((err) => {
-          res.statusMessage = err
-          res.status(400).end()
-        })
-      connection.release()
-    })
-  })
-
-
-router.route('/:userID')
-.all((req, res, next) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  next()
-})
-.get((req, res) => {
-  let sql = `SELECT ua.user_id, ua.account_type_id, ua.username, acct.account_type,acct.station_id
-  FROM user_accounts ua, account_type acct
-  where ua.account_type_id = acct.account_type_id and ua.user_id = ?`
-  //database query havent filter by date
-  pool.getConnection().then(function (connection) {
-    connection.query(sql, parseInt(req.params.userID) )
-      .then((rows) => {
-        res.json(rows)
-      })
-      .catch(err => {
-        res.statusMessage = err
-        res.status(400).end()
-      })
-    connection.release()
-  })
-})
-.put((req, res) => {
-  const saltRounds = 10;
-  var salt = bcrypt.genSaltSync(saltRounds);
-  var hash = bcrypt.hashSync(req.params.password, salt);
-
-  let userData = JSON.parse(req.body.webFormData);
-  let password_hash = hash;
-  let sql = `update user_accounts set account_type_id = ?, username = ?, password_hash = ?
-  where user_id = ?`
-  let userVal = [parseInt(userData.account_type_id), userData.username, password_hash, parseInt(req.params.userID)]
-  pool.getConnection().then(function (connection) {
-    connection.query(sql, userVal)
-      .then((rows) => {
-        // console.log(rows)
-        res.end('Success')
-      })
-      .catch((err) => {
-        res.statusMessage = err
-        res.status(400).end()
-      })
-    connection.release()
-  })
-})
-.delete((req, res) => {
-  let sql = 'Select username From user_accounts where user_id = ' + req.params.userID + ';'
-  sql += 'Delete From user_accounts where user_id = ' + req.params.userID
-  pool.getConnection().then(function(connection) {
-    connection.query(sql)
-      .then((results) => {
-        res.end('Deleted Role Successfully')
-      })
-      .catch(err => {
-        res.statusMessage = err
-        res.status(400).end()
-      })
-    connection.release()
-  })
-})
 
 router.get('/getAccountTypeCrewList', (req,res) => {
   let sql = `SELECT a.*, st.station_name FROM account_type a
@@ -150,6 +69,92 @@ router.get('/getAccountTypeCrewList', (req,res) => {
   })
 })
 
+  .post((req, res) => {
+    let sql =`insert into user_accounts( account_type_id,username,password_hash)values (?,?,?)`
+    let passwordhash = "";
+    let data = JSON.parse(req.body.webFormData);
+    let userData = [parseInt(data.account_type_id),data.username,passwordhash];
+    pool.getConnection().then(function(connection) {
+      connection.query(sql, userData)
+        .then((rows) => {
+          res.json(rows)
+          res.status(200).end()
+        })
+        .catch((err) => {
+          res.statusMessage = err
+          res.status(400).end()
+        })
+      connection.release()
+    })
+  })
+
+
+
+  .put((req, res) => {
+    const saltRounds = 10;
+    var salt = bcrypt.genSaltSync(saltRounds);
+    var hash = bcrypt.hashSync(req.params.password, salt);
+
+    let userData = JSON.parse(req.body.webFormData);
+    let password_hash = hash;
+    let sql = `update user_accounts set account_type_id = ?, username = ?, password_hash = ?
+  where user_id = ?`
+    let userVal = [parseInt(userData.account_type_id), userData.username, password_hash, parseInt(req.params.userID)]
+    pool.getConnection().then(function (connection) {
+      connection.query(sql, userVal)
+        .then((rows) => {
+          // console.log(rows)
+          res.end('Success')
+        })
+        .catch((err) => {
+          res.statusMessage = err
+          res.status(400).end()
+        })
+      connection.release()
+    })
+  })
+
+
+router.route('/:userID')
+.all((req, res, next) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  next()
+})
+
+.get((req, res) => {
+  let sql = `SELECT ua.user_id, ua.account_type_id, ua.username, acct.account_type,acct.station_id
+  FROM user_accounts ua, account_type acct
+  where ua.account_type_id = acct.account_type_id and ua.user_id = ?`
+  //database query havent filter by date
+  pool.getConnection().then(function (connection) {
+    connection.query(sql, parseInt(req.params.userID) )
+      .then((rows) => {
+        res.json(rows)
+      })
+      .catch(err => {
+        res.statusMessage = err
+        res.status(400).end()
+      })
+    connection.release()
+  })
+})
+
+.delete((req, res) => {
+  let sql = 'Select username From user_accounts where user_id = ' + req.params.userID + ';'
+  sql += 'Delete From user_accounts where user_id = ' + req.params.userID
+  pool.getConnection().then(function(connection) {
+    connection.query(sql)
+      .then((results) => {
+        res.end('Deleted Role Successfully')
+      })
+      .catch(err => {
+        res.statusMessage = err
+        res.status(400).end()
+      })
+    connection.release()
+  })
+})
 
 router.get('/getListOfAccountbyAccountTypeID/:accountTypeID', function (req, res) {
   let sql = `SELECT ua.user_id, ua.account_type_id, ua.username, acct.account_type
