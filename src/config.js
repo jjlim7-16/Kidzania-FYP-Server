aeb7bfd1178fc9e2f7193bab41175c2f15fc2d9a
jjlim7-16 <jjlim7.16@ichat.sp.edu.sg>
2019-03-06 19:28:46 +0800

    update

--- src/config.js
+++ src/config.js
@@ -2,14 +2,14 @@ const os = require('os')
 
 module.exports = {
 	// Localhost
-	hostname: 'localhost'
+	hostname: 'localhost',
 
 	// KidZania Server
 	// hostname: '152.10.0.239'
 	// hostname: 'local.kidzania.com'
 
 	// Hamachi
-	hostname: '25.30.64.144'
+	// hostname: '25.30.64.144'
 
 	// Public IP Address
 	// hostname: os.networkInterfaces()['Wi-Fi'][1].address
